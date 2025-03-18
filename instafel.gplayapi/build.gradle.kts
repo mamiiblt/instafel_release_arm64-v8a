40b99c7b113d73dd75eac4c22264f76207e3e883
mamiiblt <macesgamesstudios@gmail.com>
2025-03-18 23:29:39 +0300

    add: Change versioning style to vct

--- instafel.gplayapi/build.gradle.kts
+++ instafel.gplayapi/build.gradle.kts
@@ -4,8 +4,14 @@ plugins {
     id("com.gradleup.shadow") version "8.3.6"
 }
 
+val commitHash: String by rootProject.extra
+
 group = "me.mamiiblt.instafel"
-version = "1.5"
+version = "v1.5-$commitHash"
+var releaseTag: String = "snapshot"
+
+println("commit: $commitHash")
+println("version: $version")
 
 repositories {
     mavenCentral()
@@ -24,8 +30,65 @@ application {
     mainClass = "me.mamiiblt.instafel.gplayapi.Main"
 }
 
+tasks.register("setTag") {
+    val releaseArg = project.findProperty("release") != null
+
+    if (releaseArg) {
+        releaseTag = "release"
+    }
+
+    doLast {
+        version = "$version-$releaseTag"
+        println("Version set to: $version")
+    }
+}
+
+tasks.register("clear-cache") {
+    val filesToDelete = listOf(
+        file("${project.projectDir}/build"),
+        file("${project.projectDir}/output"),
+    )
+
+    delete(filesToDelete)
+    doLast {
+        println("Cache successfully deleted.")
+    }
+
+    mustRunAfter("setTag")
+}
+
 tasks.shadowJar {
-    archiveBaseName = "instafel-gplayapi"
+    archiveBaseName = "ifl-gplayapi"
+    archiveClassifier = ""
+    destinationDirectory.set(file("${project.projectDir}/output"))
+
+    doLast {
+        println("JAR generated.")
+    }
+
+    mustRunAfter("clear-cache")
+}
+
+tasks.register("makeClear") {
+
+    doLast {
+        delete(file("${project.projectDir}/build"))
+        println("Project cleared.")
+    }
+
+    mustRunAfter("shadowJar")
+}
+
+tasks.withType<JavaCompile> {
+    options.compilerArgs.add("-Xlint:deprecation")
+}
+
+tasks.register("build-jar") {
+    dependsOn("setTag", "clear-cache", "shadowJar", "makeClear")
+
+    doLast {
+        println("All tasks completed succesfully")
+    }
 }
 
 tasks.test {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,21 @@
-rootProject.name = "instafel"
+import java.io.ByteArrayOutputStream
+
+rootProject.name = "me.mamiiblt.instafel"
+
+fun getGitCommitHash(): String {
+    val output = ByteArrayOutputStream()
+    exec {
+        commandLine("git", "rev-parse", "--short", "HEAD")
+        standardOutput = output
+    }
+    return output.toString().trim()
+}
+
+val commitHash = getGitCommitHash()
+
+gradle.rootProject {
+    extensions.extraProperties["commitHash"] = commitHash
+}
 
 rootDir.listFiles()
     .filter { it.isDirectory && it.name.startsWith("instafel") && File(it, "build.gradle.kts").exists() }
